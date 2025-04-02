9c15b44243b480b67236c85247dd2b62f3071afd
PicarUn <christianpicar23@gmail.com>
2025-04-02 20:09:52 +0800

    UPDATED
    Moved Css & Js to folders

---
+++ Scripts/Payment.js
@@ -0,0 +1,29 @@
+function showPaymentFields() {
+    let selectedMethod = document.querySelector('input[name="payment-method"]:checked');
+    let paymentDetails = document.getElementById("payment-details");
+    paymentDetails.innerHTML = "";
+    if (!selectedMethod) return;
+    
+    if (selectedMethod.value === "credit-card") {
+        paymentDetails.innerHTML = `
+            <label>Card Number:</label>
+            <input type="text" name="card-number" required><br>
+            <label>Expiration Date:</label>
+            <input type="text" name="expiration-date" required><br>
+            <label>CVV:</label>
+            <input type="text" name="cvv" required><br>
+        `;
+    } else if (selectedMethod.value === "paypal") {
+        paymentDetails.innerHTML = `
+            <label>PayPal Email:</label>
+            <input type="email" name="paypal-email" required><br>
+        `;
+    } else if (selectedMethod.value === "bank-transfer") {
+        paymentDetails.innerHTML = `
+            <label>Bank Account Number:</label>
+            <input type="text" name="bank-account" required><br>
+            <label>Bank Name:</label>
+            <input type="text" name="bank-name" required><br>
+        `;
+    }
+}
