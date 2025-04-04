846cc99b3ecef222dd091b4165aec638f0fb881e
PicarUn <christianpicar23@gmail.com>
2025-04-04 20:03:36 +0800

    Update reservation_sys.sql

--- reservation_sys.sql
+++ reservation_sys.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2025 at 12:47 PM
+-- Generation Time: Apr 04, 2025 at 02:03 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,19 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `apt_info` (
-  `Apt_id` int(11) NOT NULL,
-  `Fname` varchar(64) NOT NULL,
-  `Lname` varchar(64) NOT NULL,
-  `Email` varchar(128) NOT NULL,
-  `Contact_num` int(15) NOT NULL,
-  `Address` varchar(255) NOT NULL,
-  `Emergency_fullname` varchar(128) NOT NULL,
-  `Emergency_num` int(15) NOT NULL,
-  `Btype` varchar(3) NOT NULL CHECK (`Btype` in ('A+','A-','B+','B-','AB+','AB-','O+','O-')),
-  `Gender` char(1) NOT NULL CHECK (`Gender` in ('M','F','O')),
+  `id` int(11) NOT NULL,
+  `Fname` varchar(50) NOT NULL,
+  `Lname` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Contact_num` varchar(20) NOT NULL,
+  `Address` text NOT NULL,
+  `Emergency_fullname` varchar(100) NOT NULL,
+  `Emergency_num` varchar(20) NOT NULL,
+  `Btype` varchar(3) NOT NULL,
+  `Gender` char(1) NOT NULL,
   `Birthdate` date NOT NULL,
-  `Med_condition` text NOT NULL,
-  `Reservation` datetime NOT NULL
+  `Med_condition` varchar(100) NOT NULL,
+  `Reservation` datetime NOT NULL,
+  `payment_method` varchar(20) NOT NULL,
+  `payment_details` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -51,7 +54,7 @@ CREATE TABLE `apt_info` (
 -- Indexes for table `apt_info`
 --
 ALTER TABLE `apt_info`
-  ADD PRIMARY KEY (`Apt_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -61,7 +64,7 @@ ALTER TABLE `apt_info`
 -- AUTO_INCREMENT for table `apt_info`
 --
 ALTER TABLE `apt_info`
-  MODIFY `Apt_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
