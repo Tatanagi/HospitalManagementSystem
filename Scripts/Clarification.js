847679f77f492ec84b06b86670a8438c4334f0c6
PicarUn <christianpicar23@gmail.com>
2025-04-06 00:52:43 +0800

    Updated
    Amogus

--- Scripts/Clarification.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let params = new URLSearchParams(window.location.search);
-    let paymentDetails = "";
-    params.forEach((value, key) => {
-        paymentDetails += `<p><strong>${key.replace("-", " ")}:</strong> ${value}</p>`;
-    });
-    document.getElementById("selected-method").innerHTML = paymentDetails || "No payment method selected.";
-});
